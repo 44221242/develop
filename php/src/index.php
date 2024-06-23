365ef28d76899a5ad0c72a45223b474c81751837
MUHAMMAD IQBAL LUTHFI DALIMUNTHE <iqballuthfi191@gmail.com>
2024-06-23 21:15:46 +0700

    Initial commit

---
+++ php/src/index.php
@@ -0,0 +1,27 @@
+<?php
+$servername = "mysql";
+$username = "user";
+$password = "password";
+$dbname = "mydb";
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Memeriksa koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully to MySQL";
+
+$sql = "SELECT id, name FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
